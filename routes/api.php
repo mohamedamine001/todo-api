4d696dad3053e3dd1f4cda4bcb4cc02a1f8ac8e4
med.amine <mohamed.amine.aloui@gmail.com>
2021-09-07 17:50:32 +0100

    Creation of API Routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::prefix('user')->group(function (){
+    Route::post('register',[UserController::class,'register']);
+    Route::post('login',[UserController::class,'login']);
+
+    Route::middleware('auth:api')->group(function () {
+       Route::get('/',[UserController::class,'user']);
+       Route::get('logout',[UserController::class,'logout']);
+
+        // todos
+       Route::resource('todos',TodoController::class);
+    });
+
 });
